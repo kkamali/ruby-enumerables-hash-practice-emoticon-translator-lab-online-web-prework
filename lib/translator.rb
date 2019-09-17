91345cb5a882136607f939fe032ef8412dabc09a
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-17 02:25:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(path)
   library = YAML.load_file(path)
   emoticons = {get_meaning: {}, get_emoticon: {}}
   library.each_pair{|key, value|
-    emoticons[:get_meaning][value[1]] = []
+    emoticons[:get_meaning][value[1]] = key
     # value.map{|emoticon|
     #   emoticons[:get_meaning][emoticon] = key
     # }
